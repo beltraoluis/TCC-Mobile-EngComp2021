e35b657c48a07593f6ed61444fd6a5e84e875373
Luís Henrique Beltrão Santana <beltraoluis@gmail.com>
2022-01-23 21:07:40 -0300

    data base creation

---
+++ docker/postgres/create.sql
@@ -0,0 +1,12 @@
+create table if not exists message (
+	id bigserial primary key,
+	platform varchar,
+	protocol varchar,
+	message_size integer,
+	send_time timestamp,
+	return_time timestamp,
+	interval_time integer,
+	message_delta integer,
+	messages_sent integer,
+	messages_received integer,
+)
